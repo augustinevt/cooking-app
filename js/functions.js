5fcc0e77551f380d268ab5aab2547933a06bfd90
August von Trapp and Wolfgang Warneke <wolfgang@>
2016-05-31 11:18:31 -0700

    add basic file tree, add unit conversion functions and basic html

---
+++ js/functions.js
@@ -0,0 +1,35 @@
+//Celsius to Fahrenheit
+function c2fTemp(celsiusTemp) {
+  return celsiusTemp * 1.8 + 32;
+}
+
+
+//Fahrenheit to Celsius
+function f2cTemp(fahrenheitTemp) {
+  return  (fahrenheitTemp - 32) / 1.8;
+}
+
+// testing
+// console.log(c2fTemp(0));
+// console.log(f2cTemp(32));
+
+function gal2lit(gal) {
+  return gal * 4.546;
+}
+
+function lit2gal(lit) {
+  return lit / 4.546;
+}
+
+function gal2qt(gal) {
+  return gal * 4;
+}
+
+function qt2gal(qt) {
+  return qt / 4;
+}
+
+
+
+
+// console.log(qt2gal(3));
